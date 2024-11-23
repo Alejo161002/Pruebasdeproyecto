e357c5a6b37d48bca571010b1ca9f159c57b6609
sebas <sebas@Sebas>
2024-11-23 14:00:57 -0600

    prueba

--- src/pruebadegifthub/PruebadeGifthub.java
+++ src/pruebadegifthub/PruebadeGifthub.java
@@ -14,7 +14,7 @@ public class PruebadeGifthub {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int hola;
     }
     
 }
